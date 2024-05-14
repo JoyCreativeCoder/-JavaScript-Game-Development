084e491ea00915c88bf42f5b8b83d912707a1143
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2024-05-14 12:55:45 +0100

    creatd new files, ready to start developing

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas1');
+    const context = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+})
